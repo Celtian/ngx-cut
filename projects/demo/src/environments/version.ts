046090e7b39eb51b555845443a874069e9828008
Dominik Hladík <dominik.hladik@seznam.cz>
2021-03-18 22:07:01 +0100

    chore(deps): angular cli updated

--- projects/demo/src/environments/version.ts
+++ projects/demo/src/environments/version.ts
@@ -1,7 +1,7 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-  TAG: '1.0.25',
+  TAG: '1.0.26',
   COMMIT_HASH: ''
 };
 /* tslint:enable */

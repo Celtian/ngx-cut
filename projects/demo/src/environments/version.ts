c054f16c5b42e32be503aef1c24d12d97363e2e8
Dominik Hladík <dominik.hladik@seznam.cz>
2021-12-03 23:32:59 +0100

    feat(angular): angular updated

--- projects/demo/src/environments/version.ts
+++ projects/demo/src/environments/version.ts
@@ -1,7 +1,7 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-  TAG: '1.0.30',
+  TAG: '1.1.0',
   COMMIT_HASH: ''
 };
 /* tslint:enable */

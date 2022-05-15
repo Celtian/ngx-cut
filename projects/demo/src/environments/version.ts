6d87a5c518a117b6b5db6484fc67c9c292da36e9
Dominik Hladík <dominik.hladik@seznam.cz>
2022-05-15 23:44:25 +0200

    chore(update): deps updated

--- projects/demo/src/environments/version.ts
+++ projects/demo/src/environments/version.ts
@@ -1,7 +1,7 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-  TAG: '2.0.0',
+  TAG: '2.0.1',
   COMMIT_HASH: ''
 };
 /* tslint:enable */

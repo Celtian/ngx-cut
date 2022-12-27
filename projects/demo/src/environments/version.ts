cc031d37b9ceedc49d32c9ca40257dea3cd3ef7c
Dominik Hladík <dominik.hladik@seznam.cz>
2022-12-27 10:15:03 +0100

    feat(community-standards): community standards added

--- projects/demo/src/environments/version.ts
+++ projects/demo/src/environments/version.ts
@@ -1,7 +1,7 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-  TAG: '2.0.2',
+  TAG: '2.0.3',
   COMMIT_HASH: ''
 };
 /* tslint:enable */
